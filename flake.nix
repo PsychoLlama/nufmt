ff0a4eadbb6ee9e709f135fddd0d394ecdd9d827
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-06 20:14:54 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      systems,
+    }:
+
+    let
+      inherit (nixpkgs) lib;
+
+      overlays = [ (import rust-overlay) ];
+
+      eachSystem = lib.flip lib.mapAttrs (
+        lib.genAttrs (import systems) (
+          system:
+          import nixpkgs {
+            inherit system overlays;
+          }
+        )
+      );
+    in
+
+    {
+      devShell = eachSystem (
+        system: pkgs:
+        pkgs.mkShell {
+          packages = [
+            (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            pkgs.just
+          ];
+        }
+      );
+    };
+}
